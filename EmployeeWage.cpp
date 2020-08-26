6c73897653a79e144d0fa8b1e0a52d593972586a
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-26 10:48:07 +0530

    REFACTORED : Code to add class and functions

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,35 +1,58 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
-int main() {
-	const int WAGE_PER_HOUR = 20;
-	const int IS_FULL_TIME = 1;
-	const int IS_PART_TIME = 2;
-	const int WORKING_DAYS = 100;
-	
-    int hour, status, totalWage = 0,totalWorkHours = 0;
-	
-    srand(time(NULL));
+class EmployeeWage{
+public:
+    int getHour(int);
+    int calculateTotalWage();  
+};
 
-	for(int day = 1; day <= WORKING_DAYS; day++) {
-		status = ((1 + rand() % 3));
-		switch(status) {
-			case IS_FULL_TIME :
-				hour = 8;
+int EmployeeWage :: getHour(int status) {
+    const  int IS_FULL_TIME = 1;
+	const  int  IS_PART_TIME = 2;
+
+    int hour = 0;
+
+    switch(status) {
+	    case IS_FULL_TIME:
+			hour = 8;
 				break;
-			case IS_PART_TIME :
-				hour = 4;
+			case IS_PART_TIME:
+			    hour = 4;
 				break;
 			default :
 				hour = 0;
-				break;
-		}
-		totalWage += hour * WAGE_PER_HOUR;
-		totalWorkHours += hour;
+			    break;
+    }      
+    return hour;
+}
 
-		if(totalWorkHours == 100) {
-         cout << "\nWage for month = " << totalWage;
-         return 0;
-      }
-	}
+int EmployeeWage :: calculateTotalWage() {
+    int hour, status;
+    const int WAGE_PER_HOUR = 20;
+	const int WORKING_DAYS = 100;
+	
+    int totalWage = 0,totalWorkHours = 0;
+
+    srand(time(NULL));
+
+    for(int day = 1; day <= WORKING_DAYS; day++) {
+	    status = ((1 + rand() % 3));
+        hour = getHour(status); 
+
+		totalWage += hour * WAGE_PER_HOUR;
+	    totalWorkHours += hour;
+
+        if(totalWorkHours == 100) {
+            cout << "\nWage for month = " << totalWage;
+            return 0;
+        } 
+    }
+        return 0;
+}
+
+int main() {
+    EmployeeWage emp;
+    emp.calculateTotalWage();
 }
