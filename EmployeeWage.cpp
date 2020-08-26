cf20f5270ee9eab9e57d7fe69c18a72c0a8db296
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-26 09:18:25 +0530

    ADDED : Code to add switch case

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -5,23 +5,26 @@ int main() {
 	const int WAGE_PER_HOUR = 20;
 	const int IS_FULL_TIME = 1;
 	const int IS_PART_TIME = 2;
+	const int DAYS_IN_MONTH = 20;
 
-	int hour, status,empWage;
+	int hour, status, totalWage = 0;
 
 	srand(time(NULL));
-	status = ((1 + rand() % 3));
 
-	if(status == IS_FULL_TIME) {
-		hour = 8;
+	for(int day = 1; day <= DAYS_IN_MONTH; day++) {
+		status = ((1 + rand() % 3));
+		switch(status) {
+			case IS_FULL_TIME :
+				hour = 8;
+				break;
+			case IS_PART_TIME :
+				hour = 4;
+				break;
+			default :
+				hour = 0;
+				break;
+		}
+		totalWage += hour * WAGE_PER_HOUR;
 	}
-	else if(status == IS_PART_TIME) {
-		hour = 4;
-	}
-	else {
-		hour = 0;
-	}
-
-	empWage = hour * WAGE_PER_HOUR;
-	cout << "\nEmployee Wage = " << empWage;
-
+	cout << "\nWage for month = " << totalWage;
 }
