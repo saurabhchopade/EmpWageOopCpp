479ce72edbe0fa6a706aea1d8ce5911eb28857f2
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-26 09:29:11 +0530

    ADDED : Code to calculate wage upto total hours reached

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -5,15 +5,14 @@ int main() {
 	const int WAGE_PER_HOUR = 20;
 	const int IS_FULL_TIME = 1;
 	const int IS_PART_TIME = 2;
-	const int DAYS_IN_MONTH = 20;
+	const int WORKING_DAYS = 100;
+	
+    int hour, status, totalWage = 0,totalWorkHours = 0;
+	
+    srand(time(NULL));
 
-	int hour, status, totalWage = 0;
-
-	srand(time(NULL));
-
-	for(int day = 1; day <= DAYS_IN_MONTH; day++) {
+	for(int day = 1; day <= WORKING_DAYS; day++) {
 		status = ((1 + rand() % 3));
-
 		switch(status) {
 			case IS_FULL_TIME :
 				hour = 8;
@@ -26,6 +25,11 @@ int main() {
 				break;
 		}
 		totalWage += hour * WAGE_PER_HOUR;
+		totalWorkHours += hour;
+
+		if(totalWorkHours == 100) {
+         cout << "\nWage for month = " << totalWage;
+         return 0;
+      }
 	}
-	cout << " \nWage for a month = " << totalWage;
 }
