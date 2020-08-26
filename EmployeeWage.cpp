9083b6f59ee0e1d77e9fcdda60eb2abf49e655c6
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-26 08:55:47 +0530

    ADDED : Code to calculate daily wage

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-    int status = ((1 + rand() % 2));
-    
-    (status == 1) ? cout << "Employee is Present": cout << "Employee is absent";
+	const int WAGE_PER_HOUR = 20;
+
+	int hour, status,empWage;
+
+	srand(time(NULL));
+	status = ((1 + rand() % 2));
+
+	if( status == 1 ) {
+		cout << "Employee is present";
+		hour = 8;
+	}
+	else {
+		cout << "Employee is absent";
+		hour = 0;
+	}
+
+	empWage = hour * WAGE_PER_HOUR;
+	cout << "\nEmployee Wage = " << empWage;
 }
